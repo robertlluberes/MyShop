95040b83da7eea0758f6d1e35fcb7bfd3704702a
robertlluberes <robertlluberes@gmail.com>
2017-12-11 14:53:48 -0400

    Dependency Injection with IRepository and Unity.MVC

--- MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Runtime.Caching;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
