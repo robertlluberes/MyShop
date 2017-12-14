81633a4ce454c8a65c5754b99b421ab8fd0dcb4d
robertlluberes <robertlluberes@gmail.com>
2017-12-14 11:57:01 -0400

    Added the bootstrap theme lumen, inproved the index page (list of products to shop)

--- MyShop.WebUI/App_Start/BundleConfig.cs
+++ MyShop.WebUI/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace MyShop.WebUI
 {
@@ -24,7 +23,7 @@ namespace MyShop.WebUI
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.lumen.css",
                       "~/Content/site.css"));
         }
     }
